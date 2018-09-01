62c137db68622f8371680a7ca10d581058c6db96
_DevD <10431310+devd@users.noreply.github.com>
2018-09-01 22:24:20 +0530

    XMLParsing
    XML Parsing

--- web/AccessingNet/AccessInter.go
+++ web/AccessingNet/AccessInter.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+type siteMapIndex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
+
 func main() {
-	http.HandleFunc("/", index_handler)
-	http.ListenAndServe(":8000", nil)
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	/*string_body := string(bytes)
+	fmt.Println(resp)
+	fmt.Println(string_body)*/
+	resp.Body.Close()
+
+	var s siteMapIndex
+	xml.Unmarshal(bytes, &s)
+	fmt.Println(s.Locations)
 }
